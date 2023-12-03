8175d9b4e9103b6cc9bfe22802db374fb6bbfa29
Sumeet Gupta <sumeetgupta3690@gmail.com>
2023-12-03 21:20:42 +0530

    OnClick open Page Feature added

--- public/js/main.js
+++ public/js/main.js
@@ -38,9 +38,13 @@ document.getElementById("userName").innerHTML= "Hello, " + localStorage.getItem(
 document.getElementById("userName1").innerHTML= "Hello, " + localStorage.getItem("UserName") + "!";
 
 
-function openMenuBarPhone(){
+let currentPage;
 
-    document.getElementById("homePage1").style.display = "none";
+function openMenuBarPhone(currentId){
+
+    currentPage = currentId;
+
+    document.getElementById(currentId).style.display = "none";
 
     document.getElementById("sideBarPhone1").style.marginLeft = "0%"
 
@@ -49,8 +53,128 @@ function openMenuBarPhone(){
 function closeMenuBar(){
 
     setTimeout(() => {
-        document.getElementById("homePage1").style.display = "block";
+        document.getElementById(currentPage).style.display = "block";
     }, 500);
 
     document.getElementById("sideBarPhone1").style.marginLeft = "-80%"
+}
+
+function openThemePage(){
+
+    
+    document.getElementById("homePage1").style.display = "none"
+    document.getElementById("themePage1").style.display = "block"
+    document.getElementById("catPage1").style.display = "none"
+    document.getElementById("anaPage1").style.display = "none"
+    document.getElementById("settPage1").style.display = "none"
+
+    document.getElementById("pmenuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem1").style.backgroundColor = "#eb06ff";
+    document.getElementById("pmenuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem4").style.backgroundColor = "#2f3046";
+
+    document.getElementById("menuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem1").style.backgroundColor = "#eb06ff";
+    document.getElementById("menuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem4").style.backgroundColor = "#2f3046";
+
+
+
+}
+function openCatPage(){
+
+    
+    document.getElementById("homePage1").style.display = "none"
+    document.getElementById("themePage1").style.display = "none"
+    document.getElementById("catPage1").style.display = "block"
+    document.getElementById("anaPage1").style.display = "none"
+    document.getElementById("settPage1").style.display = "none"
+
+    document.getElementById("pmenuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem2").style.backgroundColor = "#eb06ff";
+    document.getElementById("pmenuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem4").style.backgroundColor = "#2f3046";
+
+    document.getElementById("menuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem2").style.backgroundColor = "#eb06ff";
+    document.getElementById("menuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem4").style.backgroundColor = "#2f3046";
+
+
+}
+function openAnaPage(){
+
+    
+    document.getElementById("homePage1").style.display = "none"
+    document.getElementById("themePage1").style.display = "none"
+    document.getElementById("catPage1").style.display = "none"
+    document.getElementById("anaPage1").style.display = "block"
+    document.getElementById("settPage1").style.display = "none"
+
+    document.getElementById("pmenuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem3").style.backgroundColor = "#eb06ff";
+    document.getElementById("pmenuItem4").style.backgroundColor = "#2f3046";
+
+    document.getElementById("menuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem3").style.backgroundColor = "#eb06ff";
+    document.getElementById("menuItem4").style.backgroundColor = "#2f3046";
+
+
+
+}
+function openSettPage(){
+
+    
+    document.getElementById("homePage1").style.display = "none"
+    document.getElementById("themePage1").style.display = "none"
+    document.getElementById("catPage1").style.display = "none"
+    document.getElementById("anaPage1").style.display = "none"
+    document.getElementById("settPage1").style.display = "block"
+
+    document.getElementById("pmenuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem4").style.backgroundColor = "#eb06ff";
+
+    document.getElementById("menuItem0").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem4").style.backgroundColor = "#eb06ff";
+
+
+
+}
+function openHomePage(){
+
+    
+    document.getElementById("homePage1").style.display = "block"
+    document.getElementById("themePage1").style.display = "none"
+    document.getElementById("catPage1").style.display = "none"
+    document.getElementById("anaPage1").style.display = "none"
+    document.getElementById("settPage1").style.display = "none"
+
+    document.getElementById("pmenuItem0").style.backgroundColor = "#eb06ff";
+    document.getElementById("pmenuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("pmenuItem4").style.backgroundColor = "#2f3046";
+
+    document.getElementById("menuItem0").style.backgroundColor = "#eb06ff";
+    document.getElementById("menuItem1").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem2").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem3").style.backgroundColor = "#2f3046";
+    document.getElementById("menuItem4").style.backgroundColor = "#2f3046";
+
+
+
 }
